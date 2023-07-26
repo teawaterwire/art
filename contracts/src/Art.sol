cba466dc143ccb56b2ed8ddf6f626b78ad69a702
teawaterwire <admin@teawaterwire.dev>
2023-07-26 11:47:55 +0200

    contract: first draft

---
+++ contracts/src/Art.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+
+import "solmate/tokens/ERC1155.sol";
+import "solmate/auth/Owned.sol";
+
+contract Art is ERC1155, Owned {
+    uint256 public constant ART_SUPPLY = 99;
+
+    uint256 public nextArtId;
+
+    mapping(uint256 => uint256) public mintedAmount;
+
+    mapping(uint256 => string) public uris;
+
+    error SoldOut(uint256 artId);
+    error AlreadyMinted(uint256 artId);
+    error ArtDoesNotExist(uint256 artId);
+
+    constructor() Owned(msg.sender) {}
+
+    function mint(uint256 artId) external {
+        if (artId >= nextArtId) revert ArtDoesNotExist(artId);
+        if (mintedAmount[artId] >= ART_SUPPLY) revert SoldOut(artId);
+        if (balanceOf[msg.sender][artId] > 0) revert AlreadyMinted(artId);
+        ++mintedAmount[artId];
+        _mint(msg.sender, artId, 1, "");
+    }
+
+    function uri(uint256 id) public view override returns (string memory) {
+        return uris[id];
+    }
+
+    function addArt(string calldata name, string calldata image) external onlyOwner {
+        string memory artURI = string(
+            abi.encodePacked(
+                "data:application/json;base64,",
+                Base64.encode(bytes(abi.encodePacked('{"name":"', name, '","image":"', image, '"}')))
+            )
+        );
+        uris[nextArtId] = artURI;
+        ++nextArtId;
+    }
+}
