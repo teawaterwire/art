37d080c07ca23cbbb212518fa8bf77a33daf92ab
teawaterwire <admin@teawaterwire.dev>
2023-07-28 12:55:45 +0200

    trigger cf

--- src/app/config.cljs
+++ src/app/config.cljs
@@ -6,11 +6,10 @@
    :support-id "support"})
 
 (def art-contract "0x8af0db174ea3aec3e1f7b4f83c80a0415cbb036c")
+(def max-mint 99)
 
 (def nftscan-key "dbDZKo4OMjVmjAlEuCGIU8A8")
 
 (def walletconnect-project-id "cb59d1d18fe7fb643719503a797a93fc")
 
 (def gnosis-chain "eip155:100")
-
-(def max-mint 99)
